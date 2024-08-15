8173815a4be4f5fd72eda4a791f879b652894737
Umar Ghaffar <umar@devconst.com>
2024-08-16 00:09:10 +0500

    unit test completion

--- src/components/Sidebar.test.tsx
+++ src/components/Sidebar.test.tsx
@@ -3,10 +3,23 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Sidebar from './Sidebar';
 import useUserData from '../hooks/useUserData';
+import "./../matchMedia";
 
 jest.mock('../hooks/useUserData');
 
+const mockUsedNavigate = jest.fn();
+const mockUsedLocation = {
+    pathname: '/blogs/all' // Provide a default pathname
+};
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockUsedNavigate,
+    useLocation: () => mockUsedLocation,
+}));
+
 describe('Sidebar', () => {
+
     const mockedUser = {
         id: 1,
         name: 'John Doe',
@@ -17,12 +30,11 @@ describe('Sidebar', () => {
         (useUserData as jest.Mock).mockReturnValue(mockedUser);
     });
 
-    it('fetches and displays user details', async () => {
+    it('fetches and displays user details', () => {
         render(<Sidebar />);
 
         expect(screen.getByText(/Hello/i)).toBeInTheDocument();
         expect(screen.getByText(mockedUser.name)).toBeInTheDocument();
-
         expect(screen.getByAltText('User Image')).toHaveAttribute('src', `https://robohash.org/${mockedUser.id}`);
     });
 });
